663b441bb7e327590a963972f8767b9ba1ad6909
Tony Edgin <tedgin@iplantcollaborative.org>
2017-12-01 14:14:43 -0700

    Squashed 'irods-netcdf-build/' changes from 2cfec17..2cc6dfa
    2cc6dfa DS-217 linted dockerfiles
    f88cabf DS-217 switch to exec form of CMD
    be1c28d DS-217 fixing image generation warninngs
    c5a2f92 DS-217 fixed array assignment bug in build
    d4a7e3f DS-217 converted body of build-packages.sh to function
    faeea24 DS-217 clean no longer errors when no images to remove
    c8c6eb8 DS-217 build and clean can now be run from anywhere
    2352cf0 DS-217 broke the build packages into its own function.
    04c1a39 DS-217 clean removes generated images
    e8537f6 DS-217 renamed comtainer exec script to build-packages
    538a702 DS-217 renamed build and clean
    55da17b DS-217 anded header doc to build.sh and clean.sh
    252e82a DS-217 ignore ls errors during bug fix phase
    1add13e DS-217 can now specify which OS to build for from the command line
    dccfcc1 Removing OpenSUSE support
    3d72c26 DS-217 only builds NetCDF artifacts that need to be built
    
    git-subtree-dir: irods-netcdf-build
    git-subtree-split: 2cc6dfac632e06aa8cb529c839b0557dc8332b22

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 
-readonly OS="$1"
 
-mkdir --parents /packages/"$OS"
+main()
+{
+  local os="$1"
 
-for pkg in api microservices icommands
-do
-  /src/"$pkg"/packaging/build.sh clean
-  /src/"$pkg"/packaging/build.sh -r
-  cp /src/"$pkg"/build/* /packages/"$OS"
-done
+  mkdir --parents /packages/"$os"
 
-readonly LibDir=/usr/lib
+  for pkg in api microservices icommands
+  do
+    /src/"$pkg"/packaging/build.sh -r
+    cp --update /src/"$pkg"/build/* /packages/"$os"
+  done
 
-case "$OS"
-in
-  centos-6)
-    pkgId=centos6
-    ;;
-  centos-7)
-    pkgId=centos7
-    ;;
-  opensuse-13)
-    pkgId=suse
-    ;;
-  ubuntu-12)
-    pkgId=ubuntu12
-    ;;
-  ubuntu-14)
-    pkgId=ubuntu14
-    ;;    
-esac
+  local pkgId=
 
-# Gather irods-runtime contents
-mkdir --parents /src/tar/irods/externals
-cp --no-dereference "$LibDir"/libirods*.so* /src/tar
-cp --no-dereference "$LibDir"/irods/externals/*.so* /src/tar/irods/externals
-tar --create --gzip --directory /src/tar --file /packages/"$OS"/irods-runtime-4.1.10-"$pkgId".tgz .
-rm --force --recursive /src/tar
+  case "$os"
+  in
+    centos-6)
+      pkgId=centos6
+      ;;
+    centos-7)
+      pkgId=centos7
+      ;;
+    ubuntu-12)
+      pkgId=ubuntu12
+      ;;
+    ubuntu-14)
+      pkgId=ubuntu14
+      ;;    
+  esac
+
+  # Gather irods-runtime contents
+  mkdir --parents /src/tar/irods/externals
+  cp --no-dereference --update /usr/lib/libirods*.so* /src/tar
+  cp --no-dereference --update /usr/lib/irods/externals/*.so* /src/tar/irods/externals
+
+  tar --create --gzip \
+      --directory /src/tar --file /packages/"$os"/irods-runtime-4.1.10-"$pkgId".tgz \
+      .
+}
+
+
+set -e
+
+main "$*"
